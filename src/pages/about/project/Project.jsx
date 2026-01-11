5fc28e7633d94c1968a3be70386279608e7319b8
V1DevX <drokigame.12@gmail.com>
2026-01-11 22:25:16 +0600

    Data (Project): Add UI translator

--- src/pages/about/project/Project.jsx
+++ src/pages/about/project/Project.jsx
@@ -1,55 +1,96 @@
 import React from "react";
-import { useLanguage } from "../../../contexts/LanguageContext";
-import img from "../../../assets/photoofall.jpg";
+import { useLanguage } from "@/contexts/LanguageContext";
+import img from "@/assets/photoofall.jpg"; // TODO: Delete
 
 const Project = () => {
   const { language } = useLanguage();
+
+  const UI = {
+    en: {
+      aboutTheProject: "ABOUT THE PROJECT",
+      jashStory: [
+        `Jash Story is an educational project that combines the study of
+        Kyrgyzstan's history with world history. We organize school
+        olympiads, develop preparation materials, and provide access to
+        useful books. Our website will become a platform where everyone
+        can find in-depth information and resources for effective
+        learning.`,
+        <br/>,<br/>,
+        
+        `The project was created by teenagers passionate about history, who
+        decided to share their knowledge with others. Starting with an
+        Instagram page, they received enormous support, which motivated
+        them to continue.`,
+        <br/>,<br/>,
+        
+        `Many team members are winners and prize-winners of various history
+        olympiads, which confirms the high level of their knowledge.`,
+        <br/>,<br/>,
+        
+        `Jash Story offers unique and useful materials accessible to
+        everyone who wants to deepen their knowledge and better understand
+        history.`,
+      ],
+    },
+    ru: {
+      aboutTheProject: "О ПРОЕКТЕ",
+      jashStory: [
+        `Jash Story — это образовательный проект, который объединяет
+        изучение истории Кыргызстана и мировой истории. Мы организуем
+        школьные олимпиады, разрабатываем материалы для подготовки и
+        предоставляем доступ к полезным книгам. Наш сайт станет
+        платформой, на которой каждый сможет найти углубленную информацию
+        и ресурсы для эффективного обучения.`,
+        <br/>,<br/>,
+        
+        `Проект был создан подростками, увлеченными историей, которые
+        решили делиться своими знаниями с другими. Начав с
+        инстаграм-страницы, они получили огромную поддержку, что стало
+        мотивом для продолжения работы.`,
+        <br/>,<br/>,
+        
+        `Многие участники команды — победители и призеры различных олимпиад
+        по истории, что подтверждает высокий уровень их знаний.`,
+        <br/>,<br/>,
+        
+        `Jash Story предлагает уникальные и полезные материалы, доступные
+        для всех, кто хочет углубить свои знания и лучше понять историю.`,
+      ],
+    },
+    kg: {
+      aboutTheProject: "ДОЛБООР ЖƟНYНДƟ", // Using special symbols, font still works.
+      jashStory: [
+        `Jash Story – бул кыргыз жана дүйнөлүк тарыхты изилдөөнү айкалыштырган
+        билим берүү долбоору. Биз мектеп олимпиадаларын уюштурабыз, даярдык
+        материалдарын иштеп чыгабыз жана пайдалуу китептерге мүмкүнчүлүк беребиз.
+        Биздин веб-сайт ар бир адам натыйжалуу окутуу үчүн терең маалымат жана
+        ресурстарды таба турган платформага айланат.`,
+        <br/>,<br/>,
+        
+        `Долбоор тарыхка кызыккан жана өз билимдерин башкалар менен бөлүшүүнү
+        чечкен өспүрүмдөр тарабынан түзүлгөн. Алар Instagram баракчасы менен
+        баштаганда, эбегейсиз колдоого ээ болуп, ишин улантууга түрткү беришкен.`,
+        <br/>,<br/>,
+        
+        `Команданын көптөгөн мүчөлөрү ар кандай тарыхый олимпиадалардын жеңүүчүлөрү
+        жана байге ээлери болуп, өздөрүнүн жогорку билим деңгээлин тастыктап жатышат.`,
+        <br/>,<br/>,
+        
+        `Jash Story билимин тереңдетип, тарыхты жакшыраак түшүнгүсү келген ар бир
+        адам үчүн жеткиликтүү болгон уникалдуу жана пайдалуу материалдарды сунуштайт.`,
+      ],
+    },
+  }
+  const { aboutTheProject, jashStory } = UI[language];
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-10 my-5 md:my-10 px-4 md:px-10">
       <div className="grid grid-cols-1 md:gap-3 xl:gap-8">
         <h2 className="font-unbounded mb-3 text-2xl md:text-4xl xl:text-5xl font-medium">
-          {language === "en" ? "ABOUT THE PROJECT" : "О ПРОЕКТЕ"}
+          { aboutTheProject }
         </h2>
         <p className="text-[#4A4A4A] font-sfpro text-sm lg:text-sm xl:text-lg">
-          {language === "en" ? (
-            <>
-              Jash Story is an educational project that combines the study of
-              Kyrgyzstan's history with world history. We organize school
-              olympiads, develop preparation materials, and provide access to
-              useful books. Our website will become a platform where everyone
-              can find in-depth information and resources for effective
-              learning. <br /> <br />
-              The project was created by teenagers passionate about history, who
-              decided to share their knowledge with others. Starting with an
-              Instagram page, they received enormous support, which motivated
-              them to continue. <br /> <br />
-              Many team members are winners and prize-winners of various history
-              olympiads, which confirms the high level of their knowledge.{" "}
-              <br /> <br />
-              Jash Story offers unique and useful materials accessible to
-              everyone who wants to deepen their knowledge and better understand
-              history.
-            </>
-          ) : (
-            <>
-              Jash Story — это образовательный проект, который объединяет
-              изучение истории Кыргызстана и мировой истории. Мы организуем
-              школьные олимпиады, разрабатываем материалы для подготовки и
-              предоставляем доступ к полезным книгам. Наш сайт станет
-              платформой, на которой каждый сможет найти углубленную информацию
-              и ресурсы для эффективного обучения. <br /> <br />
-              Проект был создан подростками, увлеченными историей, которые
-              решили делиться своими знаниями с другими. Начав с
-              инстаграм-страницы, они получили огромную поддержку, что стало
-              мотивом для продолжения работы. <br /> <br />
-              Многие участники команды — победители и призеры различных олимпиад
-              по истории, что подтверждает высокий уровень их знаний. <br />{" "}
-              <br />
-              Jash Story предлагает уникальные и полезные материалы, доступные
-              для всех, кто хочет углубить свои знания и лучше понять историю.
-            </>
-          )}
+          { jashStory }
         </p>
       </div>
       <div>
