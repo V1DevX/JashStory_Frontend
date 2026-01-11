2b01e573c4f0ee353ecf4fca309d14892c58ab22
V1DevX <drokigame.12@gmail.com>
2026-01-11 21:07:06 +0600

    Fix (Card): Link instead of useNavigate

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,5 +1,5 @@
 // import { useInView } from "react-intersection-observer";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Card as UICard } from "@/components/ui/card";
 import cloudinaryTransforms from "@/features/cloudinaryTransforms";
 import { useLanguage } from "../../contexts/LanguageContext";
@@ -17,7 +17,6 @@ const UI = {
 }
 
 const Card = ({ id, img, text, bgColor = "bg-white" }) => {
-  const navigate = useNavigate();
   const { language } = useLanguage()
 
   const finalImage = cloudinaryTransforms(img, [
@@ -26,31 +25,30 @@ const Card = ({ id, img, text, bgColor = "bg-white" }) => {
     "c_fill",       // crop
   ]);
 
-  const handleArticleClick = () => {
-    navigate(`/article/${id}`);
-  };
-
   return (
-    <UICard 
-      className={`relative rounded-2xl overflow-hidden shadow-sm hover:shadow-md transition cursor-pointer transition-transform duration-300 hover:scale-[101%] ${bgColor}`}
-      onClick={handleArticleClick}
-    >
-      <div className="w-full h-48 sm:h-56 md:h-52 lg:h-56 overflow-hidden">
-        <img 
-          src={finalImage}
-          alt={text}
-          className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
-        />
-      </div>
+    <Link to={`/article/${id}`} className="block">
+      <UICard 
+        className={`relative rounded-2xl overflow-hidden shadow-sm hover:shadow-md transition cursor-pointer transition-transform duration-300 hover:scale-[101%] ${bgColor}`}
+      >
+        <div className="w-full h-48 sm:h-56 md:h-52 lg:h-56 overflow-hidden">
+          <img 
+            src={finalImage}
+            alt={text}
+            className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
+          />
+        </div>
 
-      <div className="p-4">
-        <h3 className="text-lg font-semibold line-clamp-2">{text}</h3>
-        <button className="absolute top-2 right-2 bg-white hover:bg-gray-100 text-gray-800 px-3 py-1 rounded">
-          {UI[language].read}
-        </button>
-      </div>
-      
-    </UICard>
+        <div className="p-4">
+          <h3 className="text-lg font-semibold line-clamp-2">{text}</h3>
+          <div
+            className="absolute top-2 right-2 bg-white hover:bg-gray-100 text-gray-800 px-3 py-1 rounded text-sm font-medium"
+          >
+            {UI[language].read}
+          </div>
+        </div>
+
+      </UICard>
+    </Link>
   )
 };
 
